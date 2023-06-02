488c0b0021c8124cfe9e3e348987c114cd38e092
Ronaldo Acevedo Ojeda <ronaldoa.ojeda@gmail.com>
2023-06-02 02:26:58 -0600

    Second Try

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/Bazaritov3-1.war
--- target/generated-sources/annotations/domain/Usuario_.java
+++ target/generated-sources/annotations/domain/Usuario_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-06-02T02:41:18")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-06-02T03:26:14")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
